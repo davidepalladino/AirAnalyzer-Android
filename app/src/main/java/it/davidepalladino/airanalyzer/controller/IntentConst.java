00b7ce7db1895b38b949c4bda21a55ec51b1a4ba
davidepalladino <davidepalladino@hotmail.com>
2021-12-26 18:33:02 +0100

    Version 2.x.x

--- app/src/main/java/it/davidepalladino/airanalyzer/controller/IntentConst.java
+++
@@ -1,9 +0,0 @@
-package it.davidepalladino.airanalyzer.controller;
-
-public interface IntentConst {
-    static final String INTENT_BROADCAST = "it.davidepalladino.airanalyzer.broadcast";
-    static final String INTENT_MESSAGE_TOAST = "MESSAGE_TOAST";
-    static final String INTENT_ROOM = "ROOM";
-    static final String INTENT_MEASURE = "MEASURE";
-    static final String INTENT_USER = "USER";
-}
